1a2b3399cb4cb4d6b103d2eb279e41637877df68
SajidKhanGithub <120332198+SajidKhanGithub@users.noreply.github.com>
2022-12-13 01:35:47 +0530

    Update ContactController.java

--- 01-Mini-Project/src/main/java/in/sajid/rest/ContactController.java
+++ 01-Mini-Project/src/main/java/in/sajid/rest/ContactController.java
@@ -9,10 +9,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import in.sajid.binding.Contact;
 import in.sajid.service.ContactService;
 
+@RestController
 public class ContactController {
 	
 	@Autowired
